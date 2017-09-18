4c3d646f6eb1c3d1afc5dde1cec4a8bc3be13a0f
Ivan Puzankov <12zrus@gmail.com>
2017-09-18 21:27:47 +0300

    Fixed formatting

--- dclnt.py
+++ dclnt.py
@@ -16,12 +16,14 @@ def is_verb(word):
     pos_info = pos_tag([word])
     return pos_info[0][1] == 'VB'
 
+
 Path = ''
+
 
 def get_trees(_path, with_filenames=False, with_file_content=False):
     filenames = []
     trees = []
-    path= Path
+    path = Path
     for dirname, dirs, files in os.walk(path, topdown=True):
         for file in files:
             if file.endswith('.py'):
@@ -58,9 +60,12 @@ def get_verbs_from_function_name(function_name):
 
 def get_all_words_in_path(path):
     trees = [t for t in get_trees(path) if t]
-    function_names = [f for f in flat([get_all_names(t) for t in trees]) if not (f.startswith('__') and f.endswith('__'))]
+    function_names = [f for f in flat([get_all_names(t) for t in trees]) if
+                      not (f.startswith('__') and f.endswith('__'))]
+
     def split_snake_case_name_to_words(name):
         return [n for n in name.split('_') if n]
+
     return flat([split_snake_case_name_to_words(function_name) for function_name in function_names])
 
 
@@ -68,13 +73,19 @@ def get_top_verbs_in_path(path, top_size=10):
     global Path
     Path = path
     trees = [t for t in get_trees(None) if t]
-    fncs = [f for f in flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees]) if not (f.startswith('__') and f.endswith('__'))]
+    fncs = [f for f in
+            flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees]) if
+            not (f.startswith('__') and f.endswith('__'))]
     print('functions extracted')
     verbs = flat([get_verbs_from_function_name(function_name) for function_name in fncs])
     return collections.Counter(verbs).most_common(top_size)
+
+
 def get_top_functions_names_in_path(path, top_size=10):
     t = get_trees(path)
-    nms = [f for f in flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]) if not (f.startswith('__') and f.endswith('__'))]
+    nms = [f for f in
+           flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]) if
+           not (f.startswith('__') and f.endswith('__'))]
     return collections.Counter(nms).most_common(top_size)
 
 
