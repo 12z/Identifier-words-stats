af57601ea4a9520a8873eb01150c986c6ff8bfbf
Ivan Puzankov <12zrus@gmail.com>
2017-11-03 11:32:17 +0300

    Devided into two modules and removed unnecessary code

--- example.py
+++ example.py
@@ -1,14 +1,24 @@
+import builtins
+import django
+import pyramid as pyramid
+
 from dclnt import get_top_verbs_in_path
+import nltk
 import os
 import collections
 
-import flask, requests
+import flask
+import requests
 
-wds = []
+
+# wds = []
 projects = [
     flask,
-    requests
+    requests,
+    pyramid,
+    django,
 ]
+
 # projects = [
 #     'django',
 #     'flask',
@@ -18,10 +28,16 @@ projects = [
 #     'sqlalchemy',
 # ]
 
-for project in projects:
-    path = os.path.dirname(project.__file__)
-    # path = os.path.join('.', project)
-    wds += get_top_verbs_in_path(path)
+path = os.path.dirname(os.path.dirname(nltk.__file__))
+wds = get_top_verbs_in_path(path)
+
+# for project in projects:
+#     pack = builtins
+#     path = os.path.dirname(project.__file__)
+#     # path = os.path.join('.', project)
+#     wds += get_top_verbs_in_path(path)
+
+print(wds)
 
 top_size = 200
 print('total %s words, %s unique' % (len(wds), len(set(wds))))
