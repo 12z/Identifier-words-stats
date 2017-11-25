e21d4e265197faec96f6acc8d1c9f36caf0b4486
Ivan Puzankov <12zrus@gmail.com>
2017-11-25 14:17:00 +0300

    Refactored data extractor

--- statistics_calculator/data_extractor.py
+++ statistics_calculator/data_extractor.py
@@ -20,104 +20,85 @@ class WordLocation(Enum):
 DataItem = namedtuple('DataItem', ['word', 'part_of_speech', 'location'])
 
 
-def _read_file_content(filename: str) -> Union[str, None]:
-
-    if not filename.endswith('.py'):
-        return
-
-    with open(filename, 'r', encoding='utf-8') as file:
-        try:
-            return file.read()
-        except SyntaxError:
-            pass  # ignoring files with invalid python syntax
+def get_data(path):
+    sources = _read_sources_from_path(path)
+    yield from _get_marked_words_from_sources(sources)
 
 
 def _read_sources_from_path(path: str) -> Iterable[str]:
 
     for root, dirnames, filenames in os.walk(path):
+
         for filename in filenames:
-            sources = _read_file_content(os.path.join(root, filename))
-            if sources:
-                yield sources
+
+            if not filename.endswith('.py'):
+                continue
+
+            with open(os.path.join(root, filename), 'r', encoding='utf-8') as file:
+                yield file.read()
 
 
-def _get_function_names_from_source_code(source_code: str) -> Iterable[str]:
-
-    for node in ast.walk(ast.parse(source_code)):
-        if not isinstance(node, ast.FunctionDef):
-            continue
-
-        starts_with_underscores = node.name.startswith('__')
-        ends_with_underscores = node.name.endswith('__')
-        if not (starts_with_underscores or ends_with_underscores):
-            yield node.name.lower()
-
-
-def get_data(path):
-    sources = _read_sources_from_path(path)
-    yield from get_marked_words_from_sources(sources)
-
-
-def get_marked_words_from_sources(sources):
+def _get_marked_words_from_sources(sources):
     for source_code in sources:
-        yield from get_marked_words_from_source_code(source_code)
+        yield from _get_marked_words_from_source_code(source_code)
 
 
-def get_marked_words_from_source_code(source_code):
+def _get_marked_words_from_source_code(source_code):
 
     try:
         for node in ast.walk(ast.parse(source_code)):
-            yield from find_functions_words(node)
-            yield from find_variables_words(node)
+            yield from _find_functions_words(node)
+            yield from _find_variables_words(node)
     except SyntaxError:
         pass  # ignore files with invalid syntax
 
 
-def find_functions_words(node):
+def _find_functions_words(node):
 
     if isinstance(node, ast.FunctionDef):
-        for word, pos in mark_words_in_identifier(node.name):
+        for word, pos in _mark_words_in_identifier(node.name):
             yield DataItem(word=word, part_of_speech=pos, location=WordLocation.FUNCTION)
 
 
-def find_variables_words(node):
+def _find_variables_words(node):
 
     if hasattr(node, 'target'):
-        yield from get_variables_words_from_target_subtree(node)
+        yield from _get_variables_words_from_target_subtree(node)
 
     if hasattr(node, 'targets'):
         for target in node.targets:
-            yield from get_variables_words_from_target_subtree(target)
+            yield from _get_variables_words_from_target_subtree(target)
 
 
-def get_variables_words_from_target_subtree(target):
-    for target_node in walk_target_subtree(target):
-        for word, pos in mark_words_in_identifier(target_node.id):
+def _get_variables_words_from_target_subtree(target):
+    for target_node in ast.walk(target):
+
+        if not isinstance(target_node, ast.Name):
+            continue
+
+        for word, pos in _mark_words_in_identifier(target_node.id):
             yield DataItem(word=word, part_of_speech=pos, location=WordLocation.VARIABLE)
 
 
-def walk_target_subtree(root):
-    for target_node in ast.walk(root):
-        if isinstance(target_node, ast.Name):
-            yield target_node
+def _mark_words_in_identifier(identifier):
+
+    if not (identifier.startswith('__') or identifier.endswith('__')):
+        yield from _get_marked_words_from_identifier(identifier.lower())
 
 
-def mark_words_in_identifier(identifier):
-    starts_with_underscores = identifier.startswith('__')
-    ends_with_underscores = identifier.endswith('__')
-    if not (starts_with_underscores or ends_with_underscores):
-        yield from get_marked_words_from_identifier(identifier.lower())
+def _get_marked_words_from_identifier(identifier) -> Iterable[Tuple[str, PartOfSpeech]]:
 
-
-def get_marked_words_from_identifier(identifier) -> Iterable[Tuple[str, PartOfSpeech]]:
     for word in identifier.split('_'):
-        if word:
-            part_of_speech = get_part_of_speech(word)
-            if part_of_speech:
-                yield word, part_of_speech
+
+        if not word:
+            continue
+
+        part_of_speech = _get_part_of_speech(word)
+        if part_of_speech:
+            yield word, part_of_speech
 
 
-def get_part_of_speech(word: str) -> Union[PartOfSpeech, None]:
+def _get_part_of_speech(word: str) -> Union[PartOfSpeech, None]:
     """
     Only supports verbs and nouns for now
     """
